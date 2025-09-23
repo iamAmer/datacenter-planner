95ef4f9a0e5cf053d4c84972d5b818c79b537370
amer <muhammedamer7810@gmail.com>
2025-09-23 18:14:59 +0300

    refactor: clean up keypress handling and correct comments

--- src/js/3d/scene3d.js
+++ src/js/3d/scene3d.js
@@ -171,26 +171,27 @@ function exportSceneToJson() {
 // Add event listener for keypress
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
-    case 't': // If "t" is pressed, switch to translation mode
-      transformControls.setMode('translate')
-      transformControls.showX = true
-      transformControls.showY = false
-      transformControls.showZ = true
-      break
-    case 'r': // If "r" is pressed, switch to rotation mode
-      transformControls.setMode('rotate')
-      transformControls.showX = false
-      transformControls.showY = true
-      transformControls.showZ = false
-      break
-    case 'y': // If "t" is pressed, switch to translation mode
-      transformControls.setMode('translate')
-      transformControls.showX = false
-      transformControls.showY = true
-      transformControls.showZ = false
-      break
+    case "t": // If "t" is pressed, switch to translation mode
+      setTransformMode("translate", true, false, true);
+      break;
+    case "r": // If "r" is pressed, switch to rotation mode
+      setTransformMode("rotate", false, true, false);
+      break;
+    case "y": // If "y" is pressed, switch to translation mode along Y-axis
+      setTransformMode("translate", false, true, false);
+      break;
+    default:
+      console.log(`Unhandled key: ${event.key}`);
+      break;
   }
 })
+
+function setTransformMode(mode, x, y, z) {
+  transformControls.setMode(mode);
+  transformControls.showX = x;
+  transformControls.showY = y;
+  transformControls.showZ = z;
+}
 
 window.selectImage = (imageName) => {
   const element = document.getElementById('dropdownMenuButton')
